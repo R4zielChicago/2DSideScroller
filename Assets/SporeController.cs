b12d296b973b2ea48276f194e5c4407d0c97e187
R4zielChicago <jcjc2k10@gmail.com>
2017-11-06 11:10:51 -0600

    Added more animations, and fine tuned the spore plant

---
+++ Assets/SporeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SporeController : MonoBehaviour {
+
+	public float sporeSpeedMax;
+	public float sporeSpeedMin;
+	public float sporeAngle;
+	public float sporeTorqueAngle;
+
+	Rigidbody2D sporeRb;
+
+
+
+	// Use this for initialization
+	void Start () {
+		sporeRb = GetComponent<Rigidbody2D> ();
+		sporeRb.AddForce (new Vector2(Random.Range (-sporeAngle, sporeAngle), 
+			Random.Range (sporeSpeedMin, sporeSpeedMax)), ForceMode2D.Impulse);
+		sporeRb.AddTorque (Random.Range (-sporeTorqueAngle, sporeTorqueAngle));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/shootSpore.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootSpore : MonoBehaviour {
+	public GameObject projectile;
+	public float shootTime;
+	public int chanceToShoot;
+	public Transform shootFrom;
+
+	float nextShootTime;
+	Animator cannonAnim;
+
+
+	// Use this for initialization
+	void Start () {
+		cannonAnim = GetComponentInChildren<Animator> ();
+		nextShootTime = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		 
+
+		if (other.tag == "Player" && nextShootTime < Time.time){
+
+			nextShootTime = Time.time + shootTime;
+
+			if(Random.Range (0f, 10f) >= chanceToShoot){
+
+				Instantiate (projectile, shootFrom.position, Quaternion.identity);
+				cannonAnim.SetTrigger ("CannonShoot");
+			}
+		}
+			
+	}
+}
